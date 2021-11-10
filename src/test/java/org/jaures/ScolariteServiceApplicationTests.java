d5c2a116073fac1d7e13d9f82e9d2a62486d6b45
beinjamin <jauresbeinjamin@gmail.com>
2021-11-10 20:58:32 +0100

    Creation de la class etudiants

--- src/test/java/org/jaures/ScolariteServiceApplicationTests.java
+++ src/test/java/org/jaures/ScolariteServiceApplicationTests.java
@@ -1,8 +1,21 @@
 package org.jaures;
 
+import java.util.Date;
+
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+@Entity 
 
+class Student {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String email;
+	private Date birthDate;
+	
+	
+}
 @SpringBootTest
 class ScolariteServiceApplicationTests {
 
