078352361e49394e8061be450eb92f1dd8a202c4
beinjamin <jauresbeinjamin@gmail.com>
2021-11-10 21:02:27 +0100

    Mise en place du webservice

--- src/test/java/org/jaures/ScolariteServiceApplicationTests.java
+++ src/test/java/org/jaures/ScolariteServiceApplicationTests.java
@@ -2,10 +2,21 @@ package org.jaures;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 @Entity 
+@Data @NoArgsConstructor @AllArgsConstructor @ToString
 
 class Student {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,6 +25,9 @@ class Student {
 	private String email;
 	private Date birthDate;
 	
+	
+}
+interface StudentRepository extends JpaRepository<Student,Long>{
 	
 }
 @SpringBootTest
